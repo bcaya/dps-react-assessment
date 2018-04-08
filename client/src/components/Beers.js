e463a5803d59b4ff01bdd92d638fa385437efa13
Bobby Caya <bob.caya1@gmail.com>
2018-04-08 11:00:52 -0600

    Add infinite scroll using semantic visibility

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,28 +1,49 @@
 import React from 'react'; 
 import axios from 'axios';
-import { Button, Grid, Card, List, Container, Segment } from 'semantic-ui-react';
+import { Button, Grid, Card, List, Container, Segment, Accordion, Icon, Visibility} from 'semantic-ui-react';
 import AllBeers from './AllBeers'
 
 class Beers extends React.Component { 
-  state = { beers: [] }
+  state = { beers: [], activeIndex: 0, page:1, totalPages:0 }
+  
 
   componentDidMount(){
     axios.get('/api/all_beers')
-      .then ( res => this.setState({ beers: res.data.entries }))
+      .then ( res => this.setState({ beers: res.data.entries,  totalPages: res.data.total_pages }))
   }
-  
+  onBottomVisible=()=>{
+    const page = this.state.page+1;
+    axios.get(`/api/all_beers?page=${page}&per_page=10`)
+      .then( ({data, headers}) => {
+        this.setState( state => {
+          return {beers: [...state.beers, ...data.entries], page: state.page+1}
+        })
+      })
+  }
+
   showBeers = () => { 
-      return this.state.beers.map((beer, key) => {
+      return this.state.beers.map((beer, index) => {
      return(
      <div>
+               <Card key={index}>
+          <Card.Content>
+            <Card.Header>
+              {beer? beer.name : 'Untitled'}
+            </Card.Header>
+            <Card.Description>
+              Style: {beer.style? beer.style.category.name : 'Unknown'}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className='ui two buttons'>
+                <Button basic color='blue'>View</Button>
+            </div>
+          </Card.Content>
+        </Card>
 
-    <Card color="yellow" key={beer.name}>
-      <Card.Content>
-        <Card.Header>{beer.name}</Card.Header>
-        <Card.Meta>ABV: {beer.abv}</Card.Meta>
-        <Card.Description>{beer.description}</Card.Description>
-      </Card.Content>
-    </Card>
+  
+   }
+    )
       </div>
           )
         }
@@ -35,14 +56,23 @@ class Beers extends React.Component {
 
  
 <Segment basic>
-  <Container style={{height: '80vh', width:'100vh', overflowX: 'scroll'}}>
+  <Container 
+  // style={
+    // {height: '80vh'}
+    // }
+    >
       <Grid>
         <Grid.Row>
               <Grid.Column width={8}>
-                
-                  <Card.Group stackable itemsPerRow={6}>
+              <div style={{ overflowX: 'scroll'}}>
+ <Visibility
+      once = {true}
+      continuous={true}
+      onBottomVisible={()=>this.onBottomVisible()}
+    >
                     {this.showBeers()}
-                  </Card.Group>
+    </Visibility>
+              </div>
               </Grid.Column>
              
         </Grid.Row>
